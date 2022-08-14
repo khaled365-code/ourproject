1f9fdd1acb44ae0a2ef72fff02f070a464a852db
khaled365-code <khaleddortmound123@gmail.com>
2022-08-14 21:09:56 +0200

    Initial commit

---
+++ app/src/main/java/com/khaledmohamed/am_clinics/logoActivity.kt
@@ -0,0 +1,11 @@
+package com.khaledmohamed.am_clinics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class logoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo)
+    }
+}
